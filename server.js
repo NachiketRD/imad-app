87adfe202959850dc0892e9d3f9ce6bef552ff34
nachiket.deshpande <nachiket.deshpande@onit.com>
2017-08-20 19:05:21 +0530

    Added new html file for article two and modified server.js file.

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/artical-one', function (req, res){
 })
 
 app.get('/artical-two', function (req, res){
-    res.send('Atical two has requested and will be served here!');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/artical-three', function (req, res){
